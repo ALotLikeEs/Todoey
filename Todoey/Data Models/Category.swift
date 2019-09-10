c18a5cf60da2046b6164b6bd80f1e0ff79cbb058
Esther Wango <warmbow@gmail.com>
2019-09-10 13:29:20 +0300

    Add title and category classes in data model

---
+++ Todoey/Data Models/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Ess on 10/09/2019.
+//  Copyright © 2019 Ess. All rights reserved.
+//
+
+import Foundation
---
+++ Todoey/Data Models/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ess on 10/09/2019.
+//  Copyright © 2019 Ess. All rights reserved.
+//
+
+import Foundation
